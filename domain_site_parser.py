636707421e6941f4d0fb3797529184d10abd2140
zhuoli liang <zhuoliseattle@gmail.com>
2023-08-31 13:54:02 -0700

    update

--- domain_site_parser.py
+++ domain_site_parser.py
@@ -1,47 +1,83 @@
-import time
+import traceback
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from queue import Queue
+import sys
 
-def extract_urls_with_js(url):
-    options = Options()
-    options.headless = True  # Run Chrome in headless mode
+visited_links = set()  # A set to keep track of visited links to avoid infinite loops
 
-    # Provide the path to your ChromeDriver executable
-    driver_path = "/home/zhuoli/Projects/github/zhuoli/data/chromedriver"
+def initialize_visited_links(link_file):
+    try:
+        with open(link_file, "r") as f:
+            for line in f:
+                visited_links.add(line.strip())
+        print("Initialized succeed: added links number: " + str(len(visited_links)))
+    except FileNotFoundError:
+        print("page_links.txt not found, starting with an empty set.")
 
-    # Initialize Chrome WebDriver
-    driver = webdriver.Chrome()
+def extract_urls_with_js(page_link_file, base_url):
+    url_queue = Queue()
+    url_queue.put(base_url)
 
-    # Navigate to the URL
-    driver.get(url)
+    while not url_queue.empty():
+        current_url = url_queue.get()
+        try:
+            if current_url in visited_links:
+                continue
 
-    # Optional: You could add some delay here to let the JavaScript content load, e.g.,
-    # import time
-    time.sleep(5)
+            visited_links.add(current_url)
 
-    # Grab the page source
-    page_source = driver.page_source
+            # Initialize the WebDriver
+            driver = webdriver.Chrome()
 
-    driver.quit()
-    # Parse the page source with BeautifulSoup
-    soup = BeautifulSoup(page_source, 'html.parser')
+            # Navigate to the URL
+            driver.get(current_url)
 
-    # Extract all links
-    links = [urljoin(url, a['href']) for a in soup.find_all('a', href=True)]
+            # Grab the page source
+            page_source = driver.page_source
 
-    return links
+            # Close the WebDriver
+            driver.quit()
+
+            # Parse the page source with BeautifulSoup
+            soup = BeautifulSoup(page_source, 'html.parser')
+
+            # Extract all links and convert them to absolute URLs
+            all_links = set([urljoin(current_url, a['href']) for a in soup.find_all('a', href=True)])
+
+            # Filter links to only include those that start with the base_url
+            filtered_links = [link for link in all_links if link.startswith(base_url) and link not in visited_links]
+
+            print(f"Visited: {current_url}, found {len(filtered_links)} links.")
+
+            # Add each link to the queue to be visited
+            for link in filtered_links:
+                url_queue.put(link)
+
+            # Write visited links to file
+            write_links_to_file(filtered_links, page_link_file)
+        except Exception as e:
+            print("Error on parsing: " + current_url)
+            print(f"An error occurred: {e}")
+            traceback.print_exc()
+            sys.exit(1)
+            
+
+def write_links_to_file(filtered_links,page_link_file):
+    with open(page_link_file, "a") as f:
+        for link in filtered_links:
+            f.write(f"url : {link}\n")
+
+ORACLE_DOCUMENT_URL = "https://docs.oracle.com/en-us/iaas/api" 
 
 if __name__ == "__main__":
-    target_url = "https://docs.oracle.com/en-us/iaas/api/#/"  # Replace with the URL you want to scrape
-    found_urls = extract_urls_with_js(target_url)
+    base_url = ORACLE_DOCUMENT_URL
+    page_link_file = "page_links.txt"
+    initialize_visited_links(page_link_file)
+    extract_urls_with_js(page_link_file,base_url)
     
     print("Found URLs:")
-    for url in found_urls:
+    for url in visited_links:
         print(url)
